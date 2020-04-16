098a6bcf4abb3bbe769964521f7bacfee841a09b
James <jwtankersley10@gmail.com>
2020-04-15 21:26:26 -0500

    portfolio js file

---
+++ assets/portfolio.js
@@ -0,0 +1,38 @@
+const portfolio = [
+    {
+      title: "Project 1: Gamers Unite",
+      tagline: "An informative application about upcoming gaming conventions for Esport gamers",
+      url: 
+      type: "coding"
+    },
+    {
+        title: "My Portfolio",
+        tagline: "This very page by yours truly",
+        url: 
+        type: "coding"
+      },
+      {
+        title: "Code Quiz",
+        tagline: "A JavaScript Quiz, using...JavaScript",
+        url: 
+        type: "coding"
+      },
+      {
+        title: "Weather Dashbaord",
+        tagline: "A simple weather dashboard utilizing APIs",
+        url: 
+        type: "coding"
+      },
+      {
+        title: "Eat Da Burger",
+        tagline: "An application that allows you to devour some good burgers",
+        url: ,
+        type: "coding"
+      },
+      {
+        title: "Star-Dogs",
+        tagline: "An app for your dog's very own personal horoscope",
+        url:,
+        type: "coding"
+      }
+  ];
